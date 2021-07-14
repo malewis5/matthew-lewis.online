a35c657160ca2a267b1c58bbc3a15e3ee301da28
Matthew Lewis <matthew.a.lewis7@gmail.com>
2021-07-14 10:14:44 -0400

    netlify

--- src/index.tsx
+++ src/index.tsx
@@ -10,5 +10,5 @@ ReactDOM.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log)) yo
+// to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
